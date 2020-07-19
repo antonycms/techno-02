9a4e60f7f122771511dde1e024e82f2451d632e3
Antony Santos <antonyc828@gmail.com>
2020-07-19 20:20:22 -0300

    feat: added global transition effects and styles to App
    added global transition effects and styles to App

--- src/App.vue
+++ src/App.vue
@@ -32,4 +32,35 @@ body,
   background-color: white;
   font-family: Arial, Helvetica, sans-serif;
 }
+
+.v-enter {
+  opacity: 0;
+  transform: translate3d(-20px, 0, 0);
+}
+
+.v-enter-active {
+  transition: all 0.3s;
+}
+
+.container {
+  display: grid;
+  grid-template-columns: 1fr minmax(200px, 500px);
+  gap: 20px;
+  padding: 40px 60px 0px 60px;
+}
+
+.page-title {
+  font-size: 30px;
+  font-weight: bold;
+  color: #234;
+  margin-bottom: 30px;
+}
+
+@media (max-width: 940px) {
+  .container {
+    display: flex;
+    flex-direction: column;
+    padding: 5%;
+  }
+}
 </style>
